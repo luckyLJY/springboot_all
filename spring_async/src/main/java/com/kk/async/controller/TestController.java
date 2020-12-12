d69e64c238c41ad0fd1d35b555a664ec3cba7b4f
kk <1020803064@qq.com>
2020-12-12 21:07:39 +0800

    添加异步任务模块

---
+++ spring_async/src/main/java/com/kk/async/controller/TestController.java
@@ -0,0 +1,44 @@
+package com.kk.async.controller;
+
+import com.kk.async.service.AsyncService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+/**
+ * @description:
+ * @author: Kk
+ * @create: 2020-12-12 20:14
+ **/
+@RestController
+public class TestController {
+    @Autowired
+    AsyncService asyncService;
+
+    //一个请求开启两个异步线程
+    @GetMapping("testAsync")
+    public String async() throws ExecutionException, InterruptedException {
+        long start = System.currentTimeMillis();
+        Future<String> f1 = asyncService.doOneSeconds();
+        Future<String> f2 = asyncService.doTwoSeconds();
+        while (!f1.isDone() || !f2.isDone()){
+            //当所有异步任务完成以后
+            if (f1.isDone() && f2.isDone()){
+                break;
+            }
+        }
+        long end = System.currentTimeMillis();
+        String time="总耗时："+(end-start);
+        System.out.println(Thread.currentThread().getName()+"---->"+time);
+        return "ok";
+    }
+}
