70448bcbff9b157f1f360c9e2283959f133b9ebf
kkmystery <1020803064@qq.com>
2020-12-24 18:33:24 +0800

    新增validation模块

---
+++ spring_cache_redisson/src/main/java/com/kk/redisson/config/RedisConfig.java
@@ -0,0 +1,17 @@
+package com.kk.redisson.config;
+
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author luokexiong
+ * @version 1.0 2020/12/24
+ * @since 1.0.0
+ */
+@EnableCaching
+@Configuration
+public class RedisConfig {
+
+}
