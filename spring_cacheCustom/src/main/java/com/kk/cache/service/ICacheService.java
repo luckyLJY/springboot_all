537bc6a4da104828a14a6eaa94ba44fe05a678ed
kk <1020803064@qq.com>
2021-01-03 20:09:06 +0800

    cache custom

---
+++ spring_cacheCustom/src/main/java/com/kk/cache/service/ICacheService.java
@@ -0,0 +1,22 @@
+package com.kk.cache.service;
+
+
+import com.kk.cache.entity.User;
+
+/**
+ * 注解缓存使用
+ *
+ * @author luokexiong
+ * @version 1.0 2020/12/25
+ * @since 1.0.0
+ */
+public interface ICacheService {
+
+    public void insert(User user);
+
+    public void delete(Integer id);
+
+    public User getOne(Integer id);
+
+    public User update(User user);
+}
